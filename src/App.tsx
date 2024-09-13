bf4e5565c1a59e2c8bda65605c4d2dd1380b0d77
unknown <pabloowyh251@gmail.com>
2024-09-13 19:55:03 -0300

    feat: PrimaryInput component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 import './App.css'
+import PrimaryInput from './components/Input/PrimaryInput'
 
 function App() {
+  const [email, setEmail] = useState("")
 
   return (
     <>
+      <PrimaryInput value={email} onChange={event => setEmail(event.target.value)} name= "email"/>
     </>
   )
 }
--- src/components/Input/PrimaryInput.tsx
+++ src/components/Input/PrimaryInput.tsx
@@ -1,9 +1,19 @@
-interface PrimaryInputProps {
+import { Input } from "@chakra-ui/react";
+import { ChangeEventHandler } from "react";
 
+interface PrimaryInputProps {
+    name: string,
+    value: string,
+    onChange: ChangeEventHandler<HTMLInputElement>
 }
 
-export default function PrimaryInput({}: PrimaryInputProps){
+export default function PrimaryInput({name, value, onChange}: PrimaryInputProps){
     return (
-        <></>
+        <Input 
+        variant='filled' 
+        placeholder='Filled' 
+        name={name} 
+        value={value} 
+        onChange={onChange}/>
     )
 }
